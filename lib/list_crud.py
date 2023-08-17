f305e8a64f0e69b7feaf4f40ad74aaedac711594
Winny <winny.chelangat2@student.moringaschool.com>
2023-08-17 20:44:34 +0300

    Completed lab

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,58 @@
 def create_an_empty_list():
-    return None
+    return []
 
 def create_a_list():
-    return None
+    return [1, 2, 3, 4]
 
-def add_element_to_end_of_list(l, element):
-    return None
+def add_element_to_end_of_list(lst, element):
+    lst.append(element)
+    return lst
 
-def add_element_to_start_of_list(l, element):
-    return None
+def add_element_to_start_of_list(lst, element):
+    lst.insert(0, element)
+    return lst
 
-def remove_element_from_end_of_list(l):
-    return None
+def remove_element_from_end_of_list(lst):
+    lst.pop()
+    return lst
 
-def remove_element_from_start_of_list(l):
-    return None
+def remove_element_from_start_of_list(lst):
+    del lst[0]
+    return lst
 
-def retrieve_first_element_from_list(l):
-    return None
+def retrieve_first_element_from_list(lst):
+    return lst[0]
 
-def retrieve_element_from_index(l, index):
-    return None
+def retrieve_element_from_index(lst, index):
+    return lst[index]
 
-def retrieve_last_element_from_list(l):
-    return None
+def retrieve_last_element_from_list(lst):
+    return lst[-1]
+
+# Test the functions
+empty_list = create_an_empty_list()
+print(empty_list)
+
+list_with_elements = create_a_list()
+print(list_with_elements)
+
+list_with_elements = add_element_to_end_of_list(list_with_elements, 5)
+print(list_with_elements)
+
+list_with_elements = add_element_to_start_of_list(list_with_elements, 0)
+print(list_with_elements)
+
+list_with_elements = remove_element_from_end_of_list(list_with_elements)
+print(list_with_elements)
+
+list_with_elements = remove_element_from_start_of_list(list_with_elements)
+print(list_with_elements)
+
+first_element = retrieve_first_element_from_list(list_with_elements)
+print(first_element)
+
+element_at_index = retrieve_element_from_index(list_with_elements, 2)
+print(element_at_index)
+
+last_element = retrieve_last_element_from_list(list_with_elements)
+print(last_element)
